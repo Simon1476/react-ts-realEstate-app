fc4906624addc36d462318732ead7c94023888af
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-04-25 16:46:26 +0900

    [FE]feat: Map 컴포넌트 추가

---
+++ frontend/src/components/Map/Map.tsx
@@ -0,0 +1,28 @@
+import { MapContainer } from "react-leaflet/MapContainer";
+import { TileLayer } from "react-leaflet/TileLayer";
+
+import "leaflet/dist/leaflet.css";
+import "./map.scss";
+import { CardItem } from "../../types/components/card";
+import Pin from "../Pin/Pin";
+
+const Map = ({ data }: { data: CardItem[] }) => {
+  return (
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      scrollWheelZoom={false}
+      className="map"
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {data.map((item) => (
+        <Pin item={item} key={item.id} />
+      ))}
+    </MapContainer>
+  );
+};
+
+export default Map;
