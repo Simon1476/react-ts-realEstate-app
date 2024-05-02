d66170e28d67cbaf3a781d018c305803cda87948
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-05-02 23:49:16 +0900

    [BE]feat: 사용자 등록 시 username과 email 검증 기능 구현

--- backend/controllers/auth.ts
+++ backend/controllers/auth.ts
@@ -8,6 +8,22 @@ export const register = async (req: Request, res: Response) => {
   const { username, email, password } = <RegisterUser>req.body;
 
   try {
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        OR: [
+          { username }, // Check for username
+          { email }, // Check for email
+        ],
+      },
+    });
+
+    if (existingUser) {
+      // User already exists, handle the error
+      return res
+        .status(400)
+        .json({ message: "Username or email already in use!" });
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const newUser = await prisma.user.create({
@@ -18,7 +34,6 @@ export const register = async (req: Request, res: Response) => {
       },
     });
   } catch (error) {
-    console.log(error);
     res.status(500).json({ message: "Failed to create user!" });
   }
 
@@ -39,30 +54,32 @@ export const login = async (req: Request, res: Response) => {
     if (!user)
       return res.status(401).json({ message: "Invalid Credentials! " });
     // 비밀번호 일치 유무 확인
-
     const isPasswordValid = await bcrypt.compare(password, user.password);
 
     if (!isPasswordValid)
       return res.status(401).json({ message: "Invalid Credentials!" });
     // 쿠키 토큰을 생성하여 사용자에게 전달
 
+    const age = 1000 * 60 * 60 * 24 * 7;
+
     const token = jwt.sign(
       {
         id: user.id,
       },
-      process.env.JWT_SECRET_KEY || ""
+      process.env.JWT_SECRET_KEY || "",
+      { expiresIn: age }
     );
-    const age = 1000 * 60 * 60 * 24 * 7;
+
+    const { password: userPassword, ...userInfo } = user;
     res
-      .cookie("test2", token, {
+      .cookie("token", token, {
         httpOnly: true,
         // secure: true
         maxAge: age,
       })
       .status(200)
-      .json({ message: "Login Successful" });
+      .json(userInfo);
   } catch (error) {
-    console.log(error);
     res.status(500).json({ message: "Failed to login!" });
   }
 };
