3528a6dd00245eb7d77fa1bd27de835d81a5e4ee
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-05-02 23:36:16 +0900

    [FE]feat: ProfilePage 로그아웃 기능 추가
    * feat: 로그아웃 함수 구현
    
    * feat: 로그아웃 시 localStorage 사용자 정보 제거
    
    * feat: 로그아웃 시 홈페이지로 리다이렉션

--- frontend/src/pages/ProfilePage/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage/ProfilePage.tsx
@@ -1,8 +1,21 @@
+import { useNavigate } from "react-router-dom";
 import Chat from "../../components/Chat/Chat";
 import List from "../../components/List/List";
+import apiRequest from "../../lib/apiRequest";
 import "./profilePage.scss";
 
 const ProfilePage = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await apiRequest.post("/auth/logout");
+      localStorage.removeItem("user");
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="profilePage">
       <div className="details">
@@ -13,7 +26,7 @@ const ProfilePage = () => {
           </div>
           <div className="info">
             <span>
-              Avatar:{" "}
+              Avatar:
               <img
                 src="https://cdn.pixabay.com/photo/2024/03/14/08/52/pug-8632718_1280.jpg"
                 alt=""
@@ -25,6 +38,7 @@ const ProfilePage = () => {
             <span>
               E-mail: <b>04068k@naver.com</b>
             </span>
+            <button onClick={handleLogout}>Logout</button>
           </div>
           <div className="title">
             <h1>My List</h1>
