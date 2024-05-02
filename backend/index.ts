7821c69b56140f80443224142ec82176539b2c4f
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-05-02 23:42:54 +0900

    [BE]feat: cross-origin requests위해 cors 미들웨어 사용
    * feat: @types/cors 라이브러리 설치

--- backend/index.ts
+++ backend/index.ts
@@ -1,4 +1,5 @@
 import express, { Express, Request, Response } from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import postRouter from "./routes/post";
 import authRouter from "./routes/auth";
@@ -8,6 +9,7 @@ dotenv.config();
 const app: Express = express();
 const port = 8800;
 
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
@@ -15,5 +17,5 @@ app.use("/api/posts", postRouter);
 app.use("/api/auth", authRouter);
 
 app.listen(port, () => {
-  console.log(`[server]: Server11 is running at <https://localhost>:${port}`);
+  console.log(`[server]: Server is running at <https://localhost>:${port}`);
 });
