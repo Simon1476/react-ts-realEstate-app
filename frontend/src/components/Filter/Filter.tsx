c8c460a7f3bfb02af4ba4214d65e2834014f9217
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-05-11 18:34:10 +0900

    [FE]feat: query로 Filter 컴포넌트 요소 값 업데이트

--- frontend/src/components/Filter/Filter.tsx
+++ frontend/src/components/Filter/Filter.tsx
@@ -1,12 +1,37 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./filter.scss";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-
+import { useSearchParams } from "react-router-dom";
+import { useState } from "react";
 const Filter = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [query, setQuery] = useState({
+    type: searchParams.get("type") || "",
+    city: searchParams.get("city") || "",
+    property: searchParams.get("property") || "",
+    minPrice: searchParams.get("minPrice") || "",
+    maxPrice: searchParams.get("maxPrice") || "",
+    bedroom: searchParams.get("bedroom") || "",
+  });
+
+  const handleChange = (
+    e:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setQuery({
+      ...query,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleFilter = () => {
+    setSearchParams(query);
+  };
   return (
     <div className="filter">
       <h1>
-        Search results for <b>Tyokyo</b>
+        Search results for <b>{searchParams.get("city")}</b>
       </h1>
       <div className="top">
         <div className="input-group">
@@ -16,13 +41,20 @@ const Filter = () => {
             id="city"
             name="city"
             placeholder="City Location"
+            onChange={handleChange}
+            defaultValue={query.city}
           />
         </div>
       </div>
       <div className="bottom">
         <div className="input-group">
           <label htmlFor="type">Type</label>
-          <select name="type" id="type">
+          <select
+            name="type"
+            id="type"
+            onChange={handleChange}
+            defaultValue={query.type}
+          >
             <option value="">any</option>
             <option value="buy">Buy</option>
             <option value="rent">Rent</option>
@@ -30,7 +62,12 @@ const Filter = () => {
         </div>
         <div className="input-group">
           <label htmlFor="property">Property</label>
-          <select name="property" id="property">
+          <select
+            name="property"
+            id="property"
+            onChange={handleChange}
+            defaultValue={query.property}
+          >
             <option value="">any</option>
             <option value="apartment">Apartment</option>
             <option value="house">House</option>
@@ -45,17 +82,33 @@ const Filter = () => {
             id="minPrice"
             name="minPrice"
             placeholder="Any"
+            onChange={handleChange}
+            defaultValue={query.minPrice}
           />
         </div>
         <div className="input-group">
           <label htmlFor="maxPrice">Max Price</label>
-          <input type="text" id="maxPrice" name="maxPrice" placeholder="Any" />
+          <input
+            type="number"
+            id="maxPrice"
+            name="maxPrice"
+            placeholder="Any"
+            onChange={handleChange}
+            defaultValue={query.maxPrice}
+          />
         </div>
         <div className="input-group">
           <label htmlFor="city">Bedroom</label>
-          <input type="text" id="bedRoom" name="bedRoom" placeholder="Any" />
+          <input
+            type="number"
+            id="bedRoom"
+            name="bedRoom"
+            placeholder="Any"
+            onChange={handleChange}
+            defaultValue={query.bedroom}
+          />
         </div>
-        <button>
+        <button onClick={handleFilter}>
           <FontAwesomeIcon icon={faMagnifyingGlass} />
         </button>
       </div>
