c683e099d31c84e0cb56d4437fab3d58ac1cdff6
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-05-15 17:05:04 +0900

    [FE]fix: SocketContext 제외

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,14 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.scss";
 import { AuthContextProvider } from "./context/AuthContext.tsx";
-import { SocketContextProvider } from "./context/SocketContext.tsx";
+// import { SocketContextProvider } from "./context/SocketContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthContextProvider>
-      <SocketContextProvider>
-        <App />
-      </SocketContextProvider>
+      <App />
     </AuthContextProvider>
   </React.StrictMode>
 );
